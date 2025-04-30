ad71d23bfe9cfd3100c4d9508d28633e00ac0830
Saood Imran <saood.imran@ontariotechu.net>
2025-04-30 15:05:24 -0400

    chore: Trigger deployment for renamed repo

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  // No explicit base path needed for <username>.github.io repo
   server: {
     host: "::",
     port: 8080,
